e3c6bcda93b363e815a57022b8b1875bcbb851b4
kxrxll <kovalyovkirill@gmail.com>
2022-02-12 16:39:38 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable eqeqeq */
 import React, {useState} from 'react';
-import {BrowserRouter as Router, Route, Routes, Link}  from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, NavLink}  from 'react-router-dom';
 import DriftPage from './Menu/DriftPage';
 import ForzaPage from './Menu/ForzaPage';
 import HomePage from './Menu/HomePage';
@@ -7,27 +8,22 @@ import TimeAttackPage from './Menu/TimeAttackPage';
 import './Menu/menu.css';
 
 function App() {
-  const [activeMenu, setActiveMenu] = useState(1)
-
-  function onClick(evt) {
-    setActiveMenu(evt.target.id);
-  }
 
   return (
     <Router>
       <div>
         <nav className="menu">
-          <Link id={1} className={activeMenu == 1 ? "menu__item menu__item-active" : "menu__item"} to="/" onClick={onClick}>Главная</Link>
-          <Link id={2} className={activeMenu == 2 ? "menu__item menu__item-active" : "menu__item"} to="/drift" onClick={onClick}>Дрифт-такси</Link>
-          <Link id={3} className={activeMenu == 3 ? "menu__item menu__item-active" : "menu__item"} to="/timeattack" onClick={onClick}>Time Attack</Link>
-          <Link id={4} className={activeMenu == 4 ? "menu__item menu__item-active" : "menu__item"} to="/forza" onClick={onClick}>Forza Karting</ Link>
+          <NavLink className={`menu__item`} to="/" >Главная</NavLink>
+          <NavLink className={`menu__item`} to="/drift" >Дрифт-такси</NavLink>
+          <NavLink className={`menu__item`} to="/timeattack" >Time Attack</NavLink>
+          <NavLink className={`menu__item`} to="/forza" >Forza Karting</ NavLink>
         </nav>
         <div className="page">
           <Routes>
             <Route path="/" exact element={<HomePage/>} />
-            <Route path="/drift" element={<DriftPage/>} />
-            <Route path="/timeattack" element={<TimeAttackPage/>} />
-            <Route path="/forza" element={<ForzaPage/>} />
+            <Route path="/drift" exact element={<DriftPage/>} />
+            <Route path="/timeattack" exact element={<TimeAttackPage/>} />
+            <Route path="/forza" exact element={<ForzaPage/>} />
           </Routes>
         </div>
       </div>
